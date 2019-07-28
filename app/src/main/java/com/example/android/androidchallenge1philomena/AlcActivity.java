3d278bad0a21040e0bd409715bd824b61ab9707e
Philomena Mbura <philomenambura@gmail.com>
2019-07-28 19:31:28 +0300

    ...

--- app/src/main/java/com/example/android/androidchallenge1philomena/AlcActivity.java
+++ app/src/main/java/com/example/android/androidchallenge1philomena/AlcActivity.java
@@ -1,26 +1,31 @@
 package com.example.android.androidchallenge1philomena;
 
-import android.app.ProgressDialog;
-import android.graphics.Bitmap;
-import android.support.v7.app.AppCompatActivity;
+import android.net.http.SslError;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.SslErrorHandler;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class AlcActivity extends AppCompatActivity {
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alc);
 
-        WebView webView = (WebView) findViewById(R.id.webview);
+        webView = (WebView) findViewById(R.id.webview);
 
-        WebSettings webSettings = webView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient()
+        {
+            public void onReceivedSslError(WebView webView, SslErrorHandler handler, SslError sslError)
+            {
+                handler.proceed();
+            }
+        });
 
         webView.loadUrl("https://andela.com/alc/");
-
     }
 }
